a20f1236eee3d09c6464bb269970849fab58fdc6
irene macapinlac <irenekaymac@gmail.com>
2021-07-13 23:47:29 +1000

    created add pw function

--- password-manager.py
+++ password-manager.py
@@ -1,13 +1,26 @@
 pwd = input("what is the master password? ")
 
+# Python function = 'def'
+def view():
+    pass
+# get user account and password and add into file
+def add()
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+# 'with' will automatically close the file - 'a' is a pen mode and edit, view or create new
+     with open(passwords.txt, "a") as f:
+         f.write(name + "|" + pwd)
+
+
 while True:
     mode = input("Would you like to add a new password or view existing passwords (view, add), press q to quit? ").lower
     if mode == "q":
         break
     if mode == "view":
-        pass
+        view()
     elif passwordelif mode == "add":
-        pass
+        add()
     else:
         print("Invalid mode.")
         continue
